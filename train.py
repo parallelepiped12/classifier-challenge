cdb608585cd7516ba12c0b42a9dd565cab48c86e
Thomas Gittings <t.gittings@surrey.ac.uk>
2022-09-29 15:27:17 +0100

    Initial solution with LeNet Classifier

---
+++ train.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 29 14:26:12 2022
+
+@author: Thomas
+"""
+from utils import dataloader
+from utils import networks
+import torch
+import itertools
+num_epochs = 100
+batches_per_epoch = 1000 # dataloader is infinite so there are no real epochs
+batch_size = 100
+image_size = [32, 32]
+min_objects = 0
+max_objects = 10
+
+network = networks.LeNet(num_classes=max_objects-min_objects+1)
+network.train()
+optimiser = torch.optim.SGD(network.parameters(), lr=0.01)
+loss_function = torch.nn.CrossEntropyLoss()
+
+dataset = dataloader.CountingDataset(min_objects, max_objects, image_size)
+for epoch in range(num_epochs):
+    epoch_loss = 0
+    epoch_accuracy = 0
+    dataloader = itertools.islice(torch.utils.data.DataLoader(dataset, batch_size=batch_size), 0, batches_per_epoch)
+    print(f'-------{epoch}--------')
+    for i, (images, classes) in enumerate(dataloader):
+        optimiser.zero_grad()
+        output = network(images)
+        loss = loss_function(output, classes)
+        loss.backward()
+        optimiser.step()
+        epoch_loss += loss / batches_per_epoch
+        epoch_accuracy += torch.sum(classes == torch.argmax(output, dim=1)) / (batches_per_epoch * batch_size)
+    print(epoch_loss, epoch_accuracy)
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -20,12 +20,12 @@ class CountingDataset(torch.utils.data.IterableDataset):
 
     def __next__(self):
         num_objects = random.randint(self.min_objects, self.max_objects)
-        return make_torch_image(num_objects, self.image_size)
+        return make_torch_image(num_objects, self.image_size), num_objects
 
 
 def make_torch_image(num_objects, image_size):
-    image = torch.zeros(image_size)
-    indices = torch.randperm(image_size[-1]* image_size[-2])[:num_objects]
+    image = torch.zeros(1, image_size[0], image_size[1])
+    indices = torch.randperm(image_size[0]* image_size[1])[:num_objects]
     for i in indices:
-        image[i % image_size[-2], i // image_size[-2]] = 1
+        image[0, i % image_size[0], i // image_size[0]] = 1
     return image
